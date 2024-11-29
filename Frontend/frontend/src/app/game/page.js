c36d936cdf218f0f489656a0211e029a4399b38c
Ashley E <136359789+AshleyJoyE@users.noreply.github.com>
2024-11-28 20:04:14 -0500

    first commit

---
+++ Frontend/frontend/src/app/game/page.js
@@ -0,0 +1,10 @@
+export default function GamePage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <main className="text-center">
+          <h1 className="text-4xl font-bold mb-4">Game Page</h1>
+          <p className="text-lg">Welcome to the game!</p>
+        </main>
+      </div>
+    );
+  }

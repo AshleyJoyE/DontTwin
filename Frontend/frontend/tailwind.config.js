e79fb7f66fcda55361a59cf63d09a01e937eccda
Ashley E <136359789+AshleyJoyE@users.noreply.github.com>
2024-11-30 09:36:56 -0500

    got gradient working and changed font

--- Frontend/frontend/tailwind.config.js
+++ Frontend/frontend/tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        questrial: ['var(--font-questrial)', 'sans-serif'],
       },
     },
   },

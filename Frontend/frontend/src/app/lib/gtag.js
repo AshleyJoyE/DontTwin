d1ca97c55d3fbfb1a70b1496e7217c03aee3d498
Ashley E <136359789+AshleyJoyE@users.noreply.github.com>
2025-02-24 18:52:20 -0500

    hopefully fixed analytics

--- Frontend/frontend/src/app/lib/gtag.js
+++ Frontend/frontend/src/app/lib/gtag.js
@@ -1,21 +1,19 @@
+'use client';
+
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  if (typeof window.gtag === 'function') {
-    window.gtag('config', GA_TRACKING_ID, {
-      page_path: url,
-    });
-  }
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-  if (typeof window.gtag === 'function') {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    });
-  }
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
 }; 
